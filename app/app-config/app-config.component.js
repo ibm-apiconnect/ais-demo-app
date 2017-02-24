fc73b5ed3df94232c435cc00434fd4eeee73958f
Tim Baker <bakert@us.ibm.com>
2017-02-24 10:57:51 -0600

    change config screen

--- app/app-config/app-config.component.js
+++ app/app-config/app-config.component.js
@@ -12,23 +12,17 @@ angular
         localStorageService.set("urlBase", (localStorageService.get("urlBase") ? localStorageService.get("urlBase") : null));
 
         console.log("init urlBase value: " + localStorageService.get("urlBase"));
-  
-        /* Pre-Set Form Values */
-        $scope.config = {
-          host: "https://api.us.apiconnect.ibmcloud.com",
-          organization: "< YOUR ORGANIZATION NAME >",
-          catalog: "sb"
-        };
-  
+
         $scope.setConfig = function (config) {
 
           console.log("setting config");
 
-          localStorageService.set("urlBase", config.host + '/' + config.organization + '/' + config.catalog);
+          localStorageService.set("urlBase", config.urlBase);
 
           console.log("new urlBase value: " + localStorageService.get("urlBase"));
 
           $location.path('/');
+
         };
 
         $rootScope.logInOutLink = "";
